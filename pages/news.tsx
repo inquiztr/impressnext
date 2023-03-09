073b338c075a9d8442a7b3d46fff383c19b9415d
Robin Bank <robin@northerncoder.ca>
2023-03-09 12:59:30 -0500

    updated

---
+++ pages/news.tsx
@@ -0,0 +1,19 @@
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+
+export default function News() {
+  return (
+    <>
+      <Head>
+        <title>Impressionable Gifts - News</title>
+        <meta
+          name="description"
+          content="Wall Décor, Ladies Clothing, Jewellery, Accent Furniture, Accessories"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div>newszzz</div>
+    </>
+  );
+}
