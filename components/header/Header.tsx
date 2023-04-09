f80b98c23d3af20ddf99232a04073b77063a5e26
Robin Bank <robin@northerncoder.ca>
2023-04-09 10:53:21 -0400

    update next/font

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import Link from 'next/link';
 import styles from './header.module.scss';
-import { Montserrat } from '@next/font/google';
+import { Montserrat } from "next/font/google";
 import logo from '/public/impress-logo.png';
 import Image from '@/components/Image';
 
